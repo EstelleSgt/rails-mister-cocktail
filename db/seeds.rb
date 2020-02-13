66e8ff332851202d68f7fdc429cb0a91be244864
Estelle Saget <estelle.saget@live.fr>
2020-02-13 16:36:37 +0100

    Rails mister cocktail : done ! manque le css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+ingredient1 = { name: "lemon" }
+ingredient2 = { name: "ice" }
+ingredient3 = { name: "mint leaves" }
+ingredient4 = { name: "strawberry" }
+ingredient5 = { name: "sugar" }
+
+[ingredient1, ingredient2, ingredient3, ingredient4, ingredient5].each do |attributes|
+  ingredient = Ingredient.create!(attributes)
+  puts "Created #{ingredient.name}"
+end
+puts "Finished!"
